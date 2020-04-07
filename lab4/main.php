9d17b724c870a53413c5d09ddae5614c919f8b2b
CurseRED <max-cun4@yandex.ru>
2020-04-07 21:52:09 +0300

    Small fixes

--- lab4/main.php
+++ lab4/main.php
@@ -21,7 +21,7 @@
 				$string = $_GET["comment"];
 			}
 			$words = array();
-			// Выделяем все слова из запроса
+			// Выделяем все слова из запроса c помощью регулярного выражения
 			preg_match_all('/(\".+\")|(\S+)/u', $string, $words, PREG_SET_ORDER);
 			// Проверка не пустой ли запрос
 			if (count($words) == 0) {
@@ -36,49 +36,34 @@
 					} else
 						$words_to_find[] = $words[$i][0];
 				}
-				// Инициализируем окончания
-				$endings = array('ь', 'а', 'я', 'и', 'ы', 'о', 'е', 'ов', 'ей', 'ам', 'ям', 'у', 'ю', 'ой', 'ами', 'ями', 'ах', 'ях', 'ий', 'ый', 'ая', 'яя', 'ое', 'ее', 'ого', 'его', 'их', 'ых', 'ие', 'ые', 'ому', 'ему', 'им', 'ым', 'ую', 'юю', 'ешь', 'ет', 'ут', 'ют', 'ете', 'ишь', 'ит', 'им', 'ите', 'ат', 'ят');
-				function cmp($a, $b) {
-					if (strlen($a) == strlen($b)) {
-					return 0;
-					}
-					return (strlen($a) < strlen($b)) ? -1 : 1;
-				}
-				usort($endings, "cmp");
-				$temp = array();
 				// Удаление окончаний
-				for ($i = count($endings) - 1; $i >= 0; $i--) {
-					for ($j = 0; $j < count($words_to_find); $j++) {
-						if (strlen($words_to_find[$j]) > strlen($endings[$i])) {
-							if (preg_match('/'.$endings[$i].'$/u', $words_to_find[$j]) == 1) {
-								$temp[] = mb_substr($words_to_find[$j], 0, -1*strlen($endings[$i]) + 1);
-							}
+				for ($i = 0; $i < count($words_to_find); $i++) {
+					$matches = array();
+					preg_match_all('/ого|ями|ите|ишь|ете|ому|ему|ешь|его|ами|их|ых|ие|ые|им|ят|ым|ую|ет|ут|ют|ит|им|ат|юю|ее|яя|ая|ый|ий|ях|ах|ой|ое|ям|ам|ей|ов|а|ы|я|и|у|о|е|ю|ь/ui', $words_to_find[$i], $matches, PREG_OFFSET_CAPTURE);
+					for ($j = 0; $j < count($matches[0]); $j++) {
+						// Проверка что найдено окончание
+						if (mb_strlen($words_to_find[$i]) > mb_strlen($matches[0][$j][0]) && mb_strlen($words_to_find[$i]) == mb_strlen($matches[0][$j][0]) + intdiv($matches[0][$j][1], 2)) {
+							$words_to_find[$i] = mb_substr($words_to_find[$i], 0, mb_strlen($words_to_find[$i]) - mb_strlen($matches[0][$j][0]));
 						}
 					}
 				}
 				// Считывание 8-ми килобайт текста
 				$srctext = file_get_contents('text.txt', FALSE, NULL, 0, 8192);
 				echo '<p>Исходный текст: '.$srctext.'</p>';
-				// Добавление слов со всеми окончаниями в слова для поиска
-				for ($i = 0; $i < count($temp); $i++) {
-					for ($j = count($endings) - 1; $j >= 0; $j--) {
-						$words_to_find[] = $temp[$i].$endings[$j];
-					}
-				}
-				// Добавляем слова для строгого поиска в слова для поиска
-				for ($i = 0; $i < count($words_to_find_raw); $i++) {
-					$words_to_find[] = $words_to_find_raw[$i];
-				}
-				$word_regexp = $words_to_find;
-				// Создание массива регулярных выражений и текста для замены
+				$words_sum = array();
+				// Создаем регулярные выражения для нестрогого поиска
 				for ($i = 0; $i < count($words_to_find); $i++) {
-					$word_regexp[$i] = '/'.$words_to_find[$i].'/u';
-					$words_to_find[$i] = '<mark>'.$words_to_find[$i].'</mark>';
+					$words_sum[] = '/'.preg_quote($words_to_find[$i]).'(ого|ями|ите|ишь|ете|ому|ему|ешь|его|ами|их|ых|ие|ые|им|ят|ым|ую|ет|ут|ют|ит|им|ат|юю|ее|яя|ая|ый|ий|ях|ах|ой|ое|ям|ам|ей|ов|а|ы|я|и|у|о|е|ю|ь)/ui';
 				}
-				ksort($word_regexp);
-				ksort($words_to_find);
-				// Замена нужного текста
-				$srctext = preg_replace($word_regexp, $words_to_find, $srctext);
+				// Создаем регулярные выражения для строгого поиска
+				for ($i = 0; $i < count($words_to_find_raw); $i++) {
+					$words_sum[] = '/'.preg_quote($words_to_find_raw[$i]).'/ui';
+				}
+				function callback_f($matches) {
+					return '<mark>'.$matches[0].'</mark>';
+				}
+				// Замена нужного текста с помощью массива регулярных выражений
+				$srctext = preg_replace_callback($words_sum, "callback_f", $srctext);
 				echo '<p>Результат поиска: '.$srctext.'</p>';
 			}
 		?>
